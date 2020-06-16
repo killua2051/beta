3b87d5c8233fd2ce4341511bbdc3f389e31b7180
Amiel Pocholo Sobisol <absobisol@student.apc.edu.ph>
2020-06-16 14:18:53 +0800

    New Commit

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::get('documentRev', 'AuthorController@crl_view');
 Route::get('document_creation', 'DocumentCreation@creationlist');
 Route::get('creation_list','DocumentCreation@creationlist');
 Route::get('creation_request_form','DocumentCreation@index');
-//Route::post('creation_request','DocumentCreation@creation_request');
+Route::post('creation_request','DocumentCreation@creation_request');
 Route::get('form_view2/{id}', 'DocumentCreation@form_view2');
 
 
